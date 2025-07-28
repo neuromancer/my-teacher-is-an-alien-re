9da1551e2bdd5742496de4170df98bf11447a343
neuromancer <gustavo.grieco@gmail.com>
2025-07-28 21:36:38 +0200

    added more smacker code

--- src/Smacker.cpp
+++ src/Smacker.cpp
@@ -17,3 +17,53 @@ void Smacker::NextFrame()
         SmackNextFrame(this->smk);
     }
 }
+
+extern "C" char DAT_00436970[];
+
+/*
+Function: FUN_0041fcc0
+Address: 0x41FCC0
+
+PUSH ESI
+PUSH EDI
+MOV EAX,dword ptr [ECX + 0xc]
+MOV ESI,ECX
+TEST EAX,EAX
+JZ 0x4D
+MOV ECX,dword ptr [0x00436970]
+CMP byte ptr [ECX + 0x46],0x2
+JNZ 0x24
+PUSH 0x0
+MOV EDI,dword ptr [0x00440538]
+PUSH EAX
+CALL EDI
+JMP 0x2A
+MOV EDI,dword ptr [0x00440538]
+MOV EAX,dword ptr [ESP + 0xc]
+MOV ECX,dword ptr [ESI + 0xc]
+PUSH EAX
+PUSH ECX
+CALL dword ptr [0x00440520]
+MOV ECX,dword ptr [0x00436970]
+CMP byte ptr [ECX + 0x46],0x2
+JNZ 0x4D
+PUSH 0x1
+MOV EAX,dword ptr [ESI + 0xc]
+PUSH EAX
+CALL EDI
+POP EDI
+POP ESI
+RET 0x4
+*/
+void Smacker::Goto(int frame)
+{
+    if (this->smk != 0) {
+        if (DAT_00436970[0x46] == 2) {
+            SmackSoundOnOff(this->smk, 0);
+        }
+        SmackGoto(this->smk, frame);
+        if (DAT_00436970[0x46] == 2) {
+            SmackSoundOnOff(this->smk, 1);
+        }
+    }
+}
--- src/Smacker.h
+++ src/Smacker.h
@@ -14,6 +14,7 @@ struct SmackStruct {
 class Smacker {
 public:
     void NextFrame();
+    void Goto(int frame);
 private:
     int field_0x0;
     int field_0x4;
@@ -22,15 +23,16 @@ private:
 };
 
 extern "C" {
-    void __stdcall SmackClose(SmackStruct* smk);
-    void __stdcall SmackBufferClose(int handle);
-    SmackStruct* __stdcall SmackOpen(const char* name, int flags, int extra_buf);
-    void __stdcall SmackToBuffer(SmackStruct* smk, int x, int y, int pitch, int height, void* buf, int flags);
-    void __stdcall SmackNextFrame(SmackStruct* smk);
-    void __stdcall SmackGoto(SmackStruct* smk, int frame);
-    void __stdcall SmackSoundUseDirectSound(int param_1, int param_2);
-    SmackStruct* __stdcall SmackBufferOpen(int uVar1, int param_2, int param_3, int param_4, int param_5, int param_6);
-    void __stdcall SmackDoFrame(SmackStruct* smk);
+    __declspec(dllimport) void __stdcall SmackClose(SmackStruct* smk);
+    __declspec(dllimport) void __stdcall SmackBufferClose(int handle);
+    __declspec(dllimport) SmackStruct* __stdcall SmackOpen(const char* name, int flags, int extra_buf);
+    __declspec(dllimport) void __stdcall SmackToBuffer(SmackStruct* smk, int x, int y, int pitch, int height, void* buf, int flags);
+    __declspec(dllimport) void __stdcall SmackNextFrame(SmackStruct* smk);
+    __declspec(dllimport) void __stdcall SmackGoto(SmackStruct* smk, int frame);
+    __declspec(dllimport) void __stdcall SmackSoundOnOff(SmackStruct* smk, int on);
+    __declspec(dllimport) void __stdcall SmackSoundUseDirectSound(int param_1, int param_2);
+    __declspec(dllimport) SmackStruct* __stdcall SmackBufferOpen(int uVar1, int param_2, int param_3, int param_4, int param_5, int param_6);
+    __declspec(dllimport) void __stdcall SmackDoFrame(SmackStruct* smk);
 }
 
 #endif // SMACKER_H
