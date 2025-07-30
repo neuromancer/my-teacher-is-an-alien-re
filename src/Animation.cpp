426cc6b5565e4ec3737fae6997adeb821ef57bb3
neuromancer <gustavo.grieco@gmail.com>
2025-07-30 17:08:51 +0200

    reorganize animation.cpp

--- src/Animation.cpp
+++ src/Animation.cpp
@@ -21,132 +21,16 @@ extern "C" {
     void FUN_0041fcb0(int);
 }
 
-/*
-Function: Play
-Address: 0x41FA80
 
-Disassembled:
-MOV EAX,FS:[0x0]
-PUSH EBP
-MOV EBP,ESP
-PUSH -0x1
-PUSH 0x41fad9
-PUSH EAX
-MOV dword ptr FS:[0x0],ESP
-SUB ESP,0x4
-PUSH ESI
-MOV ESI,ECX
-MOV dword ptr [EBP + -0x4],0x0
-CALL 0x0041cd50
-MOV ECX,dword ptr [EBP + 0x8]
-OR dword ptr [EAX + 0x8c],ECX
-MOV ECX,EAX
-CALL 0x00420020
-MOV ECX,EAX
-MOV dword ptr [EBP + -0x4],0xffffffff
-CALL 0x0041fb70
-MOV EAX,dword ptr [EBP + -0xc]
-POP ESI
-MOV dword ptr FS:[0x0],ECX
-MOV ESP,EBP
-POP EBP
-RET 0x4
-*/
-void Animation::Play(char* filename, unsigned int flags)
+/* Function start: 0x41FCA0 */
+void Animation::DoFrame()
 {
-    Sprite::Sprite(filename);
-    this->flags |= flags;
-    this->MainLoop();
+  if (this->smack_struct != 0) {
+    SmackDoFrame(this->smack_struct);
+  }
 }
 
-/*
-Function: Open
-Address: 0x41FE50
-
-Disassembled:
-MOV EAX,FS:[0x0]
-PUSH EBP
-MOV EBP,ESP
-PUSH -0x1
-PUSH 0x420014
-PUSH EAX
-MOV dword ptr FS:[0x0],ESP
-SUB ESP,0x20
-PUSH EBX
-PUSH ESI
-PUSH EDI
-MOV dword ptr [EBP + -0x10],ECX
-MOV EAX,dword ptr [EBP + 0x8]
-MOV dword ptr [EBP + -0x4],0x0
-PUSH EAX
-LEA EAX,[EBP + -0x14]
-PUSH EAX
-CALL 0x00424c40
-ADD ESP,0x8
-MOV EAX,dword ptr [EBP + -0x14]
-TEST EAX,EAX
-JZ 0x4E
-PUSH EAX
-CALL 0x00424940
-ADD ESP,0x4
-MOV ECX,dword ptr [EBP + 0x8]
-PUSH ECX
-CALL 0x00424a00
-ADD ESP,0x4
-MOV dword ptr [EBP + -0x14],EAX
-MOV ECX,dword ptr [EBP + -0x10]
-MOV dword ptr [ECX + 0xb4],EAX
-MOV ECX,dword ptr [EBP + -0x10]
-MOV EAX,dword ptr [ECX + 0xb4]
-PUSH EAX
-CALL 0x00424980
-ADD ESP,0x4
-TEST EAX,EAX
-JNZ 0x8F
-LEA EAX,[EBP + -0x14]
-PUSH EAX
-PUSH 0x437174
-CALL 0x00419110
-MOV ECX,dword ptr [EBP + -0x10]
-MOV EAX,dword ptr [ECX + 0xb4]
-PUSH EAX
-CALL dword ptr [0x00440504]
-ADD ESP,0x4
-MOV ECX,dword ptr [EBP + -0x10]
-MOV dword ptr [ECX + 0xc],EAX
-TEST EAX,EAX
-JNZ 0xC1
-LEA EAX,[EBP + -0x14]
-PUSH EAX
-PUSH 0x4371a0
-CALL 0x00419110
-MOV ECX,dword ptr [EBP + -0x10]
-MOV EAX,dword ptr [ECX + 0xc]
-MOV EDX,dword ptr [EBP + 0xc]
-PUSH EDX
-PUSH EAX
-CALL dword ptr [0x0044051c]
-ADD ESP,0x8
-MOV ECX,dword ptr [EBP + -0x10]
-MOV EAX,dword ptr [ECX + 0xc]
-MOV dword ptr [EBP + -0x4],0x1
-MOV dword ptr [ECX + 0xd0],EAX
-MOV ECX,dword ptr [EBP + -0x10]
-CALL 0x0041d500
-MOV ECX,dword ptr [EBP + -0x10]
-CALL 0x0041fd80
-MOV ECX,dword ptr [EBP + -0x10]
-MOV EAX,dword ptr [ECX + 0x8]
-CALL 0x0041d1c0
-MOV EAX,dword ptr [EBP + -0xc]
-POP EDI
-POP ESI
-POP EBX
-MOV dword ptr FS:[0x0],EAX
-MOV ESP,EBP
-POP EBP
-RET 0xc
-*/
+/* Function start: 0x41FD20 */
 void Animation::Open(char* filename, int param_2, unsigned int param_3)
 {
     this->filename = strdup(filename);
@@ -157,12 +41,29 @@ void Animation::Open(char* filename, int param_2, unsigned int param_3)
     if (this->smack_struct == 0) {
         ShowError("Error opening smack file");
     }
-    this->animation_data = (Animation*)this->smack_struct;
+    //this->animation_data = this;
     this->CheckRanges1();
     this->VBInit();
     this->ToBuffer();
 }
 
+/* Function start: 0x41FD80 */
+void Animation::VBInit()
+{
+    try {
+        if (this->vbuffer->data != 0) {
+            ShowError("s_VBuffer_already_created_004370ac");
+        }
+        VBuffer* new_buffer = (VBuffer*)FUN_004249c0(0x30);
+        if (new_buffer != 0) {
+            new_buffer->VirtualBufferCreateAndClean(this->smack_struct->width, this->smack_struct->height);
+        }
+    } catch (...) {
+        // Catch all exceptions
+    }
+}
+
+/* Function start: 0x41FE70 */
 void Animation::ToBuffer()
 {
     if (this->smack_struct == 0) {
@@ -172,6 +73,37 @@ void Animation::ToBuffer()
     this->ToBuffer2(this);
 }
 
+/* Function start: 0x41FEA0 */
+void Animation::ToBuffer2(void* param_1)
+{
+    if (this->smack_struct == 0) {
+        ShowError("s_Animation__ToBuffer_____No_smk_d_004370e4");
+    }
+    int uVar1 = FUN_004224d0();
+    this->vbuffer->field_0x28 = uVar1;
+    SmackStruct* piVar2 = SmackBufferOpen(uVar1, 4, 4, 4, 0, 0);
+    this->smacker_buffer = piVar2;
+    if (piVar2 == 0) {
+        ShowError("s_Animation__ToBuffer_____Buffer_c_00437144");
+    }
+    if (this->vbuffer->data != 0) {
+        ShowError("s_Animation__ToBuffer_____Virtual_B_0043710c");
+    }
+    this->vbuffer->data = (VBuffer*)param_1;
+    unsigned int uVar3 = (unsigned int)*(char*)this->smacker_buffer;
+    int uVar1_2 = this->vbuffer->GetSomething();
+    SmackToBuffer(this->smack_struct, 0, 0, this->smack_struct->width * 2, this->smack_struct->height, (void*)uVar1_2, uVar3);
+}
+
+/* Function start: 0x41FF30 */
+void Animation::Play(char* filename, unsigned int flags)
+{
+    Sprite::Sprite(filename);
+    this->flags |= flags;
+    this->MainLoop();
+}
+
+/* Function starT: 0x420020 */
 void Animation::MainLoop()
 {
     if (this->smack_struct == 0) {
@@ -226,47 +158,4 @@ void Animation::MainLoop()
 
 LAB_0042012f:
     this->vbuffer->InvalidateVideoMode();
-}
-
-void Animation::VBInit()
-{
-    try {
-        if (this->vbuffer->data != 0) {
-            ShowError("s_VBuffer_already_created_004370ac");
-        }
-        VBuffer* new_buffer = (VBuffer*)FUN_004249c0(0x30);
-        if (new_buffer != 0) {
-            new_buffer->VirtualBufferCreateAndClean(this->smack_struct->width, this->smack_struct->height);
-        }
-    } catch (...) {
-        // Catch all exceptions
-    }
-}
-
-void Animation::ToBuffer2(void* param_1)
-{
-    if (this->smack_struct == 0) {
-        ShowError("s_Animation__ToBuffer_____No_smk_d_004370e4");
-    }
-    int uVar1 = FUN_004224d0();
-    this->vbuffer->field_0x28 = uVar1;
-    SmackStruct* piVar2 = SmackBufferOpen(uVar1, 4, 4, 4, 0, 0);
-    this->smacker_buffer = piVar2;
-    if (piVar2 == 0) {
-        ShowError("s_Animation__ToBuffer_____Buffer_c_00437144");
-    }
-    if (this->vbuffer->data != 0) {
-        ShowError("s_Animation__ToBuffer_____Virtual_B_0043710c");
-    }
-    this->vbuffer->data = (VBuffer*)param_1;
-    unsigned int uVar3 = (unsigned int)*(char*)this->smacker_buffer;
-    int uVar1_2 = this->vbuffer->GetSomething();
-    SmackToBuffer(this->smack_struct, 0, 0, this->smack_struct->width * 2, this->smack_struct->height, (void*)uVar1_2, uVar3);
-}
-
-void Animation::DoFrame()
-{
-  if (this->smack_struct != 0) {
-    SmackDoFrame(this->smack_struct);
-  }
 }
