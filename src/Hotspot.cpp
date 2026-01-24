0a48e0e145807a1a264019f7cc2c5a35b592a555
neuromancer <gustavo.grieco@gmail.com>
2026-01-24 14:52:55 +0100

    checkpoint

--- src/Hotspot.cpp
+++ src/Hotspot.cpp
@@ -136,26 +136,10 @@ int T_Hotspot::Update(int param_1, int param_2, int param_3)
         if (list1 == 0 || list1->DoAll() != 0) {
             state = 2;
         }
-        if (state == 2) {
-            goto case_2;
+        if (state != 2) {
+            return 0;
         }
-        return 0;
-    case 3:
-        if (list3 != 0) {
-            if (list3->DoAll() != 0) {
-                state = 1;
-                return 1;
-            }
-        }
-        else {
-            state = 1;
-            return 1;
-        }
-        return 1;
-    case 4:
-        return 1;
     case 2:
-    case_2:
         if (list2 != 0) {
             if (list2->DoAll() != 0) {
                 state = 3;
@@ -172,6 +156,20 @@ int T_Hotspot::Update(int param_1, int param_2, int param_3)
             }
         }
         return 0;
+    case 3:
+        if (list3 != 0) {
+            if (list3->DoAll() != 0) {
+                state = 1;
+                return 1;
+            }
+        }
+        else {
+            state = 1;
+            return 1;
+        }
+        return 1;
+    case 4:
+        return 1;
     }
     ShowError("Error in Thotspot.cpp - Update()");
     return 1;
